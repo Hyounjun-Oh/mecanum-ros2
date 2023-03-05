9d58093c72a4baee316d283a7a4dac50d00a30a5
HyounjunOh <ohj_980918@naver.com>
2023-03-06 00:51:55 +0900

    auto add

--- src/motor_control/motor_control/motor_control.py
+++ src/motor_control/motor_control/motor_control.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Editor : Hyoujun Oh
+
 #Arduino Mega - Master Port : /dev/ttyACM0
 import rclpy
 from rclpy.node import Node
@@ -19,22 +21,38 @@ import numpy as np
 import serial
 from geometry_msgs.msg import Twist
 import time
-
+#from rcl_interfaces.msg import Parameter
+from rclpy.parameter import Parameter
+#from rclpy.parameter import ParameterType
+#from rcl_interfaces.msg import ParameterValue
+#from rcl_interfaces.srv import SetParameters
 
 class CmdVelSubscriber(Node):
 
     def __init__(self):
         super().__init__('motor_control')
+        #self.my_param = Parameter('my_param', Parameter.Type.STRING, 'hello')
+        self.declare_parameter('mobile_robot_length', 0.15)
+        self.length = self.get_parameter('mobile_robot_length').value # 중심점으로부터 모터의 세로 위치
+        self.declare_parameter('qos_depth', 10)
+        qos_depth = self.get_parameter('qos_depth').value #qos파라미터 셋팅
+        self.declare_parameter('mobile_robot_width', 0.17)
+        self.width = self.get_parameter('mobile_robot_width').value # 중심점으로부터 모터의 가로 위치
+        self.declare_parameter('mobile_robot_radius', 0.05)
+        self.radius = self.get_parameter('mobile_robot_radius').value # 메카넘휠 반지름
+        self.declare_parameter('baudrate', 115200)
+        self.baudrate = self.get_parameter('baudrate').value #시리얼 보드레이트
+        self.declare_parameter('arduino_port', "/dev/ttyACM0")
+        self.port= self.get_parameter('arduino_port').value #시리얼 포트
+        self.declare_parameter('arduino_num', 0)
+        self.arduino_num= Parameter('arduino_num', Parameter.Type.INTEGER,1).value #시리얼 포트
         self.subscription = self.create_subscription(
             Twist,
             'cmd_vel',
             self.get_cmd_vel,
-            10)
+            qos_depth)
         self.subscription  # prevent unused variable warning
-        self.width = 0.17 # 중심점으로부터 모터의 가로 위치
-        self.length = 0.15 # 중심점으로부터 모터의 세로 위치
-        self.radius = 0.05 # 메카넘휠 반지름
-        self.ser = serial.Serial('/dev/ttyACM0', 115200) #OpenCR Port COM3, MEGE Port COM4
+        self.ser = serial.Serial(self.port, self.baudrate) #OpenCR Port COM3, MEGE Port COM4
 
     def get_cmd_vel(self, msg):
         self.Vx = msg.linear.x #m/s
@@ -42,34 +60,32 @@ class CmdVelSubscriber(Node):
         self.Rz = msg.angular.z #rad/s
         self.cmd_vel2rad()
         if self.ser.readable():
-            #for motor_num in range(1,5):
-            #    trans_data = str(motor_num) + "," + str(round(float(self.rpm_value[motor_num -1][0]),2))
-            #    trans_data = trans_data.encode()
-            #    self.ser.write(trans_data)
-            #    self.get_logger().info("Data transfer is successful!")
-            #    self.get_logger().info(trans_data)
-            trans_data = str(round(float(self.rpm_value[0][0]),2)) + "," + str(round(float(self.rpm_value[1][0]),2))
-            trans_data = trans_data.encode()
-            self.ser.write(trans_data)
-            self.get_logger().info("Data transfer is successful!")
-            self.get_logger().info(trans_data)
-        self.ser.flush()
+            if self.arduino_num == 0:                
+                trans_data = str(round(float(self.rpm_value[0][0]),2)) + "," + str(round(float(self.rpm_value[1][0]),2))
+                trans_data = trans_data.encode()
+                self.ser.write(trans_data)
+                self.get_logger().info("Front motor data transfer is successful!")
+                self.get_logger().info(trans_data)
+            if self.arduino_num == 1:
+                trans_data = str(round(float(self.rpm_value[0][0]),2)) + "," + str(round(float(self.rpm_value[1][0]),2))
+                trans_data = trans_data.encode()
+                self.ser.write(trans_data)
+                self.get_logger().info("Rear motor data transfer is successful!")
+                self.get_logger().info(trans_data)
+        self.ser.flush() # 시리얼 버퍼 초기화
         # 0.001초 딜레이 적용 완료 아두이노는 10ms 간격
-        # 시리얼 통신 실행 이전에 아두이노 초기화가 되어야함?!
         time.sleep(0.001)
-        #motor_num = 1
        
     def cmd_vel2rad(self):
-        # convert_matrix = np.array([[1, -1, -(self.width + self.length)], [1, 1, (self.width + self.length)], [1, 1, -(self.width + self.length)], [1, -1, (self.width + self.length)]])
-        w1 = (-self.Vx/self.radius) + (self.Vy/self.radius) + self.Rz*(self.length + self.width) # rad/s
-        w2 = (self.Vx/self.radius) + (self.Vy/self.radius) - self.Rz*(self.length + self.width)
-        w3 = -(self.Vx/self.radius) + (self.Vy/self.radius) - self.Rz*(self.length + self.width)
-        w4 = (self.Vx/self.radius) + (self.Vy/self.radius) + self.Rz*(self.length + self.width)
-        self.rpm_value = np.array([[w1*9.5492968],[w2*9.5492968],[w3*9.5492968],[w4*9.5492968]]) # rad/s -> rpm
+        if self.arduino_num == 0:
+            w1 = (-self.Vx/self.radius) + (self.Vy/self.radius) + self.Rz*(self.length + self.width)
+            w2 = (self.Vx/self.radius) + (self.Vy/self.radius) - self.Rz*(self.length + self.width)
+            self.rpm_value = np.array([[w1*9.5492968],[w2*9.5492968]]) # rad/s -> rpm
+        else:          
+            w3 = -(self.Vx/self.radius) + (self.Vy/self.radius) - self.Rz*(self.length + self.width)
+            w4 = (self.Vx/self.radius) + (self.Vy/self.radius) + self.Rz*(self.length + self.width)
+            self.rpm_value = np.array([[w3*9.5492968],[w4*9.5492968]]) # rad/s -> rpm
             
-            
-
-
 def main(args=None):
     rclpy.init(args=args)
 
@@ -79,8 +95,6 @@ def main(args=None):
 
     cmd_vel_sub.destroy_node()
     rclpy.shutdown()
-    ser.close()
-
 
 if __name__ == '__main__':
     main()
--- src/motor_control/setup.py
+++ src/motor_control/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import glob
+import os
 
 package_name = 'motor_control'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob.glob(os.path.join('launch', '*.launch.py'))),
+        ('share/' + package_name + '/param', glob.glob(os.path.join('param', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
