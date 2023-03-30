8eb9e898946bd3deb107931cfc22b2787ad50831
hyounjunoh <ohj_980918@naver.com>
2023-03-30 23:16:07 +0200

    auto git from ubuntu

---
+++ build/ydlidar_ros2_driver/CMakeFiles/ydlidar_ros2_driver_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ydlidar_ros2_driver_node.dir/src/ydlidar_ros2_driver_node.cpp.o"
+  "ydlidar_ros2_driver_node"
+  "ydlidar_ros2_driver_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ydlidar_ros2_driver_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
