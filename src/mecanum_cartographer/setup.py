e052ee89abd67ebd3b0a44b99fcda283468325db
HyounjunOh <ohj_980918@naver.com>
2023-04-11 15:15:20 +0900

    auto git from ubuntu

--- src/mecanum_cartographer/setup.py
+++ src/mecanum_cartographer/setup.py
@@ -11,9 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*'))
+        ('share/' + package_name + '/launch', glob.glob(os.path.join('launch', '*.launch.py'))),
+        ('share/' + package_name + '/config', glob.glob(os.path.join('config', '*.lua'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
