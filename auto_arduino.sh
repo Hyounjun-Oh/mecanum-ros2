58e0b913e371d8f9c8906bc5667ee8c33c01a71e
hyounjunoh <ohj_980918@naver.com>
2023-05-11 00:39:40 +1000

    auto git from ubuntu

---
+++ auto_arduino.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 아두이노 스케치 파일 경로
+front_sketch="mecanum-ros2/arduino_code/motor_control_front_final_float/motor_control_front_final_float.ino"
+rear_sketch="mecanum-ros2/arduino_code/motor_control_rear_final_float/motor_control_rear_final_float.ino"
+
+# 아두이노 보드 타입
+board_type_1="arduino:avr:mega"
+board_type_2="arduino:avr:mega"
+
+# 아두이노 포트
+front_port="/dev/ttyACM0"
+rear_port="/dev/ttyACM1"
+
+# 아두이노 스케치 컴파일 및 업로드
+arduino-cli compile --fqbn $board_type_2 $front_sketch
+arduino-cli upload -p $front_port -b $board_type_1 $front_sketch
+arduino-cli compile --fqbn $board_type_1 $rear_sketch
+arduino-cli upload -p $rear_port -b $board_type_2 $rear_sketch
