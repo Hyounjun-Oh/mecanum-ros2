e762a53ce07f621c28b693d32a78a13122c9cbf1
hyounjunoh <ohj_980918@naver.com>
2023-05-13 23:29:23 +1000

    auto git from ubuntu

---
+++ build/stella_ahrs/CMakeFiles/stella_ahrs_node.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stella_ahrs_node.dir/mw/mw_ahrs.cpp.o"
+  "CMakeFiles/stella_ahrs_node.dir/serial/list_ports_linux.cc.o"
+  "CMakeFiles/stella_ahrs_node.dir/serial/serial.cc.o"
+  "CMakeFiles/stella_ahrs_node.dir/serial/unix.cc.o"
+  "CMakeFiles/stella_ahrs_node.dir/src/listener.cpp.o"
+  "stella_ahrs_node"
+  "stella_ahrs_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stella_ahrs_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ install/dynamixel_sdk_custom_interfaces/include/dynamixel_sdk_custom_interfaces/msg/detail/set_position__traits.hpp
@@ -0,0 +1,76 @@
+// generated from rosidl_generator_cpp/resource/idl__traits.hpp.em
+// with input from dynamixel_sdk_custom_interfaces:msg/SetPosition.idl
+// generated code does not contain a copyright notice
+
+#ifndef DYNAMIXEL_SDK_CUSTOM_INTERFACES__MSG__DETAIL__SET_POSITION__TRAITS_HPP_
+#define DYNAMIXEL_SDK_CUSTOM_INTERFACES__MSG__DETAIL__SET_POSITION__TRAITS_HPP_
+
+#include "dynamixel_sdk_custom_interfaces/msg/detail/set_position__struct.hpp"
+#include <stdint.h>
+#include <rosidl_runtime_cpp/traits.hpp>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
+namespace rosidl_generator_traits
+{
+
+inline void to_yaml(
+  const dynamixel_sdk_custom_interfaces::msg::SetPosition & msg,
+  std::ostream & out, size_t indentation = 0)
+{
+  // member: id
+  {
+    if (indentation > 0) {
+      out << std::string(indentation, ' ');
+    }
+    out << "id: ";
+    value_to_yaml(msg.id, out);
+    out << "\n";
+  }
+
+  // member: position
+  {
+    if (indentation > 0) {
+      out << std::string(indentation, ' ');
+    }
+    out << "position: ";
+    value_to_yaml(msg.position, out);
+    out << "\n";
+  }
+}  // NOLINT(readability/fn_size)
+
+inline std::string to_yaml(const dynamixel_sdk_custom_interfaces::msg::SetPosition & msg)
+{
+  std::ostringstream out;
+  to_yaml(msg, out);
+  return out.str();
+}
+
+template<>
+inline const char * data_type<dynamixel_sdk_custom_interfaces::msg::SetPosition>()
+{
+  return "dynamixel_sdk_custom_interfaces::msg::SetPosition";
+}
+
+template<>
+inline const char * name<dynamixel_sdk_custom_interfaces::msg::SetPosition>()
+{
+  return "dynamixel_sdk_custom_interfaces/msg/SetPosition";
+}
+
+template<>
+struct has_fixed_size<dynamixel_sdk_custom_interfaces::msg::SetPosition>
+  : std::integral_constant<bool, true> {};
+
+template<>
+struct has_bounded_size<dynamixel_sdk_custom_interfaces::msg::SetPosition>
+  : std::integral_constant<bool, true> {};
+
+template<>
+struct is_message<dynamixel_sdk_custom_interfaces::msg::SetPosition>
+  : std::true_type {};
+
+}  // namespace rosidl_generator_traits
+
+#endif  // DYNAMIXEL_SDK_CUSTOM_INTERFACES__MSG__DETAIL__SET_POSITION__TRAITS_HPP_
