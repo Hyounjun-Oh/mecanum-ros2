5b217de86620791b7b6638514753f5718b678995
HyounjunOh <ohj_980918@naver.com>
2023-05-13 21:09:16 +0900

    autogit

--- src/mecanum_odometry/setup.py
+++ src/mecanum_odometry/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+import os
+import glob
 package_name = 'mecanum_odometry'
 
 setup(
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob.glob(os.path.join('launch', '*.launch.py'))),
+        ('share/' + package_name + '/param', glob.glob(os.path.join('param', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
