eaf28983d1ae0a43ad809c0669692dd98799c422
hyounjunoh <ohj_980918@naver.com>
2023-05-20 06:33:12 +1000

    auto git from ubuntu

--- src/motor_control/motor_control/motor_control_float.py
+++ src/motor_control/motor_control/motor_control_float.py
@@ -122,7 +122,7 @@ class CmdVelSubscriber(Node):
                         pass
 
     def cmd_vel2rad(self):
-        alpha = 2.25 #속도 실측에 대한 가중치 알파
+        alpha = 2.4 #속도 실측에 대한 가중치 알파
         if self.arduino_num == 0:
             self.w1 = round((((self.Vx - self.Vy - self.Rz*((self.length + self.width)/2))/self.radius)/alpha*9.5492968),2)
             self.w2 = round((((self.Vx + self.Vy + self.Rz*((self.length + self.width)/2))/self.radius)/alpha*9.5492968),2)
