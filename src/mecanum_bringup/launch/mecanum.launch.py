2dee6eb07a94648d8ff43420be2725b6bb52222c
HyounjunOh <ohj_980918@naver.com>
2023-06-16 02:44:20 +0900

    autogit

--- src/mecanum_bringup/launch/mecanum.launch.py
+++ src/mecanum_bringup/launch/mecanum.launch.py
@@ -72,7 +72,7 @@ def generate_launch_description():
         ),
         Node(
             package='manipulator',
-            executable='manipulater_move',
+            executable='manipulator_move',
             name='manipulater_move',
             output='screen'),
         Node(
