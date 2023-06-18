880d52c1faf6e9d44d0d409ed894e2040b8730a5
Hyounjun-Oh <ohj_980918@naver.com>
2023-06-18 17:43:55 +0900

    autogit

--- src/DynamixelSDK/dynamixel_sdk_examples/src/read_write_node.cpp
+++ src/DynamixelSDK/dynamixel_sdk_examples/src/read_write_node.cpp
@@ -148,8 +148,8 @@ ReadWriteNode::ReadWriteNode()
 
     }
     );
-  auto mani_flag_ = this->create_publisher<std_msgs::msg::Int16>("manipulator_flag", QOS_RKL10V);
-  auto timer_ = this->create_wall_timer(std::chrono::milliseconds(500),std::bind(&ReadWriteNode::manipulatorFlag, this));
+  mani_flag_ = this->create_publisher<std_msgs::msg::Int16>("manipulator_flag", QOS_RKL10V);
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(500),std::bind(&ReadWriteNode::manipulatorFlag, this));
   auto get_present_position =
     [this](
     const std::shared_ptr<GetPosition::Request> request,
